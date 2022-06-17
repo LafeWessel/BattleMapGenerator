7c3ada75f7301b1995e6997d6f48b53677fbf859
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2022-06-17 15:59:34 -0500

    Added new enum types and removed some to prevent from too much initial complexity

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,53 @@
 
-enum TileType{
-    Forest,
-    Hill,
-    Mountain,
-    Outpost,
-    Plains,
-    River,
-    Road,
-    Swamp,
-    Town,
-}
+mod tiles{
 
-enum AddOn{
-    Barricade,
-}
+    enum CampaignMapTileType{
+        Forest,
+        Hill,
+        Mountain,
+        Outpost,
+        Plains,
+        River,
+        Road,
+        Swamp,
+        Town,
+    }
 
-enum RiverCrossing{
-    Ford,
-    BridgeStone,
-    BridgeWood
-}
+    // TODO add any missing tile types that are in the base game
+    enum BattleMapTileType{
+        Forest,
+        Hill,
+        Mountain,
+        Outpost,
+        Plains,
+        River,
+        Road,
+        Swamp,
+        Town,
+    }
 
+    // enum RoadTileTypes{
+
+    // }
+
+    // enum RiverTileTypes{
+
+    // }
+
+    // enum AddOn{
+    //     Barricade,
+    // }
+
+    // enum RiverCrossing{
+    //     Ford,
+    //     BridgeStone,
+    //     BridgeWood
+    // }
+
+    struct MapTile{
+        b_type: BattleMapTileType,
+        // add_on: Option<AddOn>,
+        // crossing: Option<RiverCrossing>,
+    }
+
+}
