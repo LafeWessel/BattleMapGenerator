b9bdb51dfa077fcdcf7ae8d13d3440481c35171e
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2022-06-17 15:47:21 -0500

    added initial tile types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 
+enum TileType{
+    Forest,
+    Hill,
+    Mountain,
+    Outpost,
+    Plains,
+    River,
+    Road,
+    Swamp,
+    Town,
+}
 
+enum AddOn{
+    Barricade,
+}
+
+enum RiverCrossing{
+    Ford,
+    BridgeStone,
+    BridgeWood
+}
 
