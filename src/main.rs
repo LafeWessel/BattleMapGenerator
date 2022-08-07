184b26805a7c6c3509d84bf4a026cb11e8d8278a
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2022-08-07 17:02:33 -0500

    Remove comments on code

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use map_generator::battle_map::Map;
 
 fn main() {
 
-    // for i in 4..=5{
-    //     for j in 2..=5{
-    //         let m = Map::create_map(i,j);
-    //         m.print_board_tiles();
-    //     }
-    // }
+    for i in 4..=5{
+        for j in 2..=5{
+            let m = Map::create_map(i,j);
+            m.print_board_tiles();
+        }
+    }
 
-    // Map::create_default_map().print_board_tiles();
+    Map::create_default_map().print_board_tiles();
 
-    // let m = Map::create_map(4, 6, vec![]);
-    // m.print_board();
+    let m = Map::create_map(4, 6, vec![]);
+    m.print_board();
 
 
     for i in 4..=5{
